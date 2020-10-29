c9071fd12311a9bf82d39335aa09dc9547a18482
Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
2020-10-29 11:26:00 +0530

    Reset app PFDs on every request
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>

--- pfcpiface/sessions.go
+++ pfcpiface/sessions.go
@@ -27,7 +27,6 @@ func NewPFCPSessionMgr(maxRetries int) *PFCPSessionMgr {
 	return &PFCPSessionMgr{
 		rng:        rand.New(rand.NewSource(time.Now().UnixNano())),
 		maxRetries: maxRetries,
-		appPFDs:    make(map[string]appPFD),
 		sessions:   make(map[uint64]PFCPSession),
 	}
 }
